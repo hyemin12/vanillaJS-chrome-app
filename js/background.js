a8233afd307fe7291517e064392cb916e163aabe
hyemin12 <khnm31@naver.com>
2022-03-20 16:55:29 +0900

    '배경이미지추가'

--- js/background.js
+++ js/background.js
@@ -3,13 +3,15 @@ const images = [
   "bg2.jpg",
   "bg3.jpg",
   "bg4.jpg",
-  "bg5.webp",
+  "bg5.jpg",
   "bg6.jpg",
-  "bg7.png",
+  "bg7.jpg",
+  "bg8.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 document.body.style.background = `url(img/${chosenImage})`;
-document.body.style.background.size = "cover";
+document.body.style.backgroundSize = "cover";
 document.body.style.backgroundPosition = "center";
+document.body.style.backgroundRepeat = "no-repeat";
